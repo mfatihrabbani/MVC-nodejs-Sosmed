090a1a3c39079fad17a2e6d63ef831eb1dbb8f64
mfatihrabbani <mfatihrabbani@gmail.com>
2022-07-19 20:54:42 +0700

    commit index

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import usersRoute from "./src/users/usersRouter.js"
+import profileRoute from "./src/profile/profileRouter.js"
 import followersRoute from "./src/followers/followersRouter.js"
 import bodyParser from "body-parser"
+import cookieParser from "cookie-parser";
 import path from "path"
 const app = express();
 
@@ -10,8 +12,10 @@ app.set('views', "./src/views");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/", usersRoute);
+app.use("/", profileRoute);
 app.use("/users", followersRoute);
 
 app.listen(3000);
